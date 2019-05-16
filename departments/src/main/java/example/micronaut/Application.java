3eae2363f2b3a4fa8537d51044e7ac8e6bfddaef
MEKHALA <MEKHALA@DESKTOP-LO2VLSV>
2019-05-16 10:35:23 +0530

    sd

--- departments/src/main/java/example/micronaut/Application.java
+++ departments/src/main/java/example/micronaut/Application.java
@@ -1,10 +1,17 @@
 package example.micronaut;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
 
-    public static void main(String[] args) {
-        Micronaut.run(Application.class);
+    public static void main(String[] args) throws IOException {
+
+    	String contents = new String(Files.readAllBytes(Paths.get("data.txt")));
+        System.out.println(contents);
+    	Micronaut.run(Application.class);
     }
 }
